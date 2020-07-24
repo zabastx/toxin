d56223703278c5ff164c4dc082fca084e37cfacd
zabastx <zabastx@gmail.com>
2020-07-25 00:53:08 +0300

    добавлены checkbox, radio, toggle, like buttons

--- src/blocks/form/elements.js
+++ src/blocks/form/elements.js
@@ -11,3 +11,29 @@ $( function() {
     $( "#amount" ).text( $( "#slider-range" ).slider( "values", 0 ) +
       "₽ - " + $( "#slider-range" ).slider( "values", 1 ) + '₽' );
   } );
+
+let liked = false;
+let likes = 10;
+
+  $(".like-btn").click(() => {
+    if (liked) {
+      $(".like-btn").css({
+        border: '1px solid rgba(31, 32, 65, 0.25)',
+        color: 'rgba(31, 32, 65, 0.25)'
+      });
+      $(".mi-icons").text('favorite_border');
+      liked = false;
+      likes--;
+      $(".like-txt").text(likes);
+    }
+    else {
+      $(".like-btn").css({
+        border: '1px solid #BC9CFF',
+        color: '#BC9CFF'
+      });
+      $(".mi-icons").text('favorite');
+      liked = true;
+      likes++;
+      $(".like-txt").text(likes);
+    }
+  })
